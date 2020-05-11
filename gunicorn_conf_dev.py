31fea1e061499dc09f943b38915334696ec8d638
Hardik Mahipal Surana <hardiksurana01@gmail.com>
2020-05-11 12:09:41 -0700

    added /record API

--- gunicorn_conf_dev.py
+++ gunicorn_conf_dev.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "0.0.0.0:80"
+bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 threads = 2 * multiprocessing.cpu_count()
--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 import json
-# import re
+from bson import json_util
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 mongo = PyMongo(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    msg = """
+        <p> Use the below API endpoints:</p>
+        <ul>
+            <li><code>/search?keywords="KEYWORDS"</code>  - to get artifacts based on keywords</li>
+            <li><code>/record?doi="DOI"</code> - to get data for a specific artifact</li>
+        </ul>
+    """
+    return msg, 200
 
 @app.route('/search', methods=['GET'])
 def search_with_keywords():
@@ -18,32 +29,58 @@ def search_with_keywords():
     -------
     JSON
         {
-            "url": [list of URLs for matched Zenodo artifacts],
-            "length": #number of URLs in "url"
+            "length": <NUMBER OF RETURNED ARTIFACTS>
+            "artifacts": [
+                {
+                    "doi": <DOI>,
+                    "title": <TITLE>,
+                    "description": <DESCRIPTION>
+                }
+            ],
         }
     """
     if "keywords" not in request.args:
-        return 'keywords missing!', 400
+        return 'keyword(s) are missing!', 400
     
     kwrds = request.args.get('keywords')
     if kwrds == "":
-        docs = mongo.db.raw_artifacts.find({"relevance_score": {"$gt": 40}}).limit(20)
+        docs = mongo.db.relevant_artifacts.find({}).limit(20)
     else:
-        docs = mongo.db.raw_artifacts.find({"$text":{"$search": kwrds}, "relevance_score": {"$gt": 40}})
+        docs = mongo.db.relevant_artifacts.find({"$text":{"$search": kwrds}})
     
-    # pattern = r"(\d*\.\d*)\/([a-z]*)(\d*)\.(\d*)"
     artifacts = []
     for doc in docs:
-        # print("DOI = " + doc["doi"])
-        # match = re.match(pattern, doc["doi"])
         result = {
-            # "id": match.groups(),
-            "url": "https://doi.org/" + doc["doi"],
+            "doi": doc["doi"],
             "title": doc["title"],
             "description": doc["description"]
         }
         artifacts.append(result)
+    
     return {"artifacts": artifacts, "length": len(artifacts)}, 200
+
+@app.route('/record', methods=['GET'])
+def get_record_by_doi():
+    """get_record_by_doi returns specific artifact by DOI
+
+    Parameters
+    ----------
+    doi : String
+        unique DOI string for each artifact
+    
+    Returns
+    -------
+    JSON
+        document from MongoDB
+    """
+    if "doi" not in request.args:
+        return 'DOI is missing!', 404
+    
+    record = mongo.db.relevant_artifacts.find_one({"doi": request.args.get('doi')})
+    if record:
+        return json.loads(json.dumps(record,default=json_util.default)), 200
+    else:
+        return "No document found!", 404
 
 if __name__ == '__main__':    
     app.run()
