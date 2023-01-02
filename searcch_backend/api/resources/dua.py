276d57b1956eb7c4c42438df4a670c9f146f6c4a
jakshay_12 <jerry.allan.akshay@gmail.com>
2023-01-02 22:35:07 +0530

    Update dua.py

--- searcch_backend/api/resources/dua.py
+++ searcch_backend/api/resources/dua.py
@@ -22,18 +22,35 @@ class DUAResource(Resource):
                                    type=str,
                                    required=True,
                                    help='missing researchers')
+        self.reqparse.add_argument(name='research',
+                                   type=str,
+                                   required=True,
+                                   help='missing research')  
+        self.reqparse.add_argument(name='project',
+                                   type=str,
+                                   required=True,
+                                   help='missing project name')    
+        self.reqparse.add_argument(name='dataset_name',
+                                   type=str,
+                                   required=True,
+                                   help='missing dataset name')                      
         super(DUAResource, self).__init__()
 
     def get(self, artifact_group_id):
         args = self.reqparse.parse_args()
         researchers = args['researchers']
+        research = args['research']
+        project = args['project']
+        dataset_name = args['dataset_name']
         researchers = json.loads(researchers)
         dua_name = db.session.query(DUA.dua_url).join(Artifact, Artifact.provider == DUA.provider).filter(artifact_group_id == Artifact.artifact_group_id).first()[0]
         dua_file = open(f'searcch_backend/api/dua_content/{dua_name}', mode='r')
         dua_content = dua_file.read()
         dua_file.close()
         soup = BeautifulSoup(dua_content, 'html.parser')
+        
         if dua_name == 'usc_dua.md':
+
             dua_a = soup.find(id='dua_a_to_replicate').parent
             dua_a_to_replicate_og = dua_a.find(id='dua_a_to_replicate')
             dua_a_to_replicate = copy.deepcopy(dua_a_to_replicate_og)
@@ -44,8 +61,12 @@ class DUAResource(Resource):
                 to_replicate.find(id='dua_a_email').string = researcher['email']
                 to_replicate.find(id='dua_a_contact').string = researcher['number']
                 dua_a.append(to_replicate)
-            # soup.find('span', {'id': 'dua_a_name'}).text = 'University of Southern California'
-            # soup.find('span', {'id': 'dua_a_name'}).replace_with('University of Southern California')
+            
+            soup.find(id='dua_b_dataset_name').string = dataset_name
+
+            soup.find(id='dua_c_project_name').string = project
+            soup.find(id='dua_c_desc').string = research
+
         response = jsonify({"dua": str(soup)})
         response.status_code = 200
         return response
